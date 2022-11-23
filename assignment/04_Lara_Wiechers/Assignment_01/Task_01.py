2441c379067b6cf71cdb29808675833efa2787f7
Cath4548 <103303660+Cath4548@users.noreply.github.com>
2022-11-23 19:38:44 +0100

    Create Task_01.py

---
+++ assignment/04_Lara_Wiechers/Assignment_01/Task_01.py
@@ -0,0 +1,5 @@
+#task 01 - Hello World
+
+print('Hello! What is your name?') #asking for users name
+username = input() #user inputting users name
+print('Hi, ' + username + '.' + 'It is nice to meet you. Welcome to the programming course.') #welcoming user to programming course
